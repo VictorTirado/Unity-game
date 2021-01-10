31313f217d671e93fda98f2e2c6d9db4581688c2
lakaens <lakaens12@gmail.com>
2021-01-10 21:34:50 +0100

    Car Selector done

---
+++ Unity-game/Assets/car selector/ChangeCar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCar : MonoBehaviour
+{
+    public GameObject[] Cars;
+    public int selectedCar = 0;
+
+    public void NextCar()
+    {
+        Cars[selectedCar].SetActive(false);
+        selectedCar = (selectedCar + 1) % Cars.Length;
+        Cars[selectedCar].SetActive(true);
+    }
+    public void PreviousCar()
+    {
+        Cars[selectedCar].SetActive(false);
+        selectedCar--;
+        if(selectedCar < 0)
+        {
+            selectedCar += Cars.Length;
+        }
+        Cars[selectedCar].SetActive(true);
+    }
+
+
+    public void SelectCar()
+    {
+       //TODO
+    }
+   
+}
