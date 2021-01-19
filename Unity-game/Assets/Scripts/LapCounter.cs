ffc73e09ef053c299d5af481c6cfd53d75e0f310
lakaens <lakaens12@gmail.com>
2021-01-19 21:10:50 +0100

    Lap Counter and Example of Win Condition done

---
+++ Unity-game/Assets/Scripts/LapCounter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapCounter : MonoBehaviour
+{
+    public GameObject lapsCount;
+    public GameObject cameratoRotate;
+    public GameObject cameratoStop;
+    
+
+    public int lapsDown;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "ColliderBody")
+        {
+            if (Time.time > 10f)
+            {
+            Debug.Log("HIIIIIIIIIIIIIIIII");
+            lapsDown += 1;
+            }            
+        }
+        lapsCount.GetComponent<Text>().text = "" + lapsDown;
+    }
+    private void Start()
+    {
+        cameratoRotate.SetActive(false);
+    }
+    private void Update()
+    {
+        if(lapsDown == 3)
+        {
+            //TODO CONDITION WIN
+            cameratoStop.SetActive(false);
+            cameratoRotate.SetActive(true);
+            cameratoRotate.transform.Rotate(0, 0.5f, 0, Space.World);
+            
+        }
+    }
+}
