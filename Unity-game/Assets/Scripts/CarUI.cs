4f4ca4c75d2419ed09cc116802cf94cc21d9f097
AlbertCayuela <albertcayuela16@gmail.com>
2021-01-27 18:49:50 +0100

    now we know the only lap is the fastest lap

--- Unity-game/Assets/Scripts/CarUI.cs
+++ Unity-game/Assets/Scripts/CarUI.cs
@@ -8,10 +8,10 @@ public class CarUI : MonoBehaviour
 {
     GameObject text;
     GameObject s_text;
-    GameObject fast_lap;
+
     Text lap_time_text;
     Text speed_text;
-    Text fast_lap_text;
+
 
     CheckpointCheck check_point_check;
     UnityStandardAssets.Vehicles.Car.CarController car_controller;
@@ -20,19 +20,17 @@ public class CarUI : MonoBehaviour
 
     public float lap_time;
     public float current_speed;
-    public float fastest_lap = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        car_variant = transform.parent.gameObject;
+        car_variant = GameObject.FindGameObjectWithTag("Player");
         text = GameObject.Find("LapTimeText");
         s_text = GameObject.Find("SpeedText");
-        fast_lap = GameObject.Find("FastLapText");
 
         lap_time_text = text.GetComponent<Text>();
         speed_text = s_text.GetComponent<Text>();
-        fast_lap_text = fast_lap.GetComponent<Text>();
+
         check_point_check = car_variant.GetComponent<CheckpointCheck>();
         car_controller = car_variant.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
 
@@ -42,8 +40,6 @@ public class CarUI : MonoBehaviour
         lap_time = check_point_check.lap_time;
         lap_time_text.text = "LAP TIME: ";
 
-        fastest_lap = check_point_check.fast_lap;
-        fast_lap_text.text = "FAST LAP: ";
     }
 
     // Update is called once per frame
@@ -54,8 +50,5 @@ public class CarUI : MonoBehaviour
 
         current_speed = car_controller.CurrentSpeed;
         speed_text.text = ((int)current_speed).ToString() + " km/h";
-
-        fastest_lap = check_point_check.fast_lap;
-        fast_lap_text.text = "FAST LAP: " + fastest_lap.ToString("f2");
     }
 }
