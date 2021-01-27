9139d119c3b672830895d79a913452ad3ac3738f
AlbertCayuela <albertcayuela16@gmail.com>
2021-01-27 12:31:24 +0100

    speed on ui

--- Unity-game/Assets/Scripts/CarUI.cs
+++ Unity-game/Assets/Scripts/CarUI.cs
@@ -6,21 +6,33 @@ using UnityEngine.UI;
 public class CarUI : MonoBehaviour
 {
     GameObject text;
+    GameObject s_text;
     Text lap_time_text;
+    Text speed_text;
 
     CheckpointCheck check_point_check;
+    UnityStandardAssets.Vehicles.Car.CarController car_controller;
 
     GameObject car_variant;
 
     public float lap_time;
+    public float current_speed;
 
     // Start is called before the first frame update
     void Start()
     {
         car_variant = GameObject.Find("Car Variant");
         text = GameObject.Find("LapTimeText");
+        s_text = GameObject.Find("SpeedText");
+
         lap_time_text = text.GetComponent<Text>();
+        speed_text = s_text.GetComponent<Text>();
         check_point_check = car_variant.GetComponent<CheckpointCheck>();
+        car_controller = car_variant.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
+
+        current_speed = car_controller.CurrentSpeed;
+        speed_text.text = current_speed.ToString();
+
         lap_time = check_point_check.lap_time;
         lap_time_text.text = "LAP TIME: ";
     }
@@ -30,5 +42,8 @@ public class CarUI : MonoBehaviour
     {
         lap_time = check_point_check.lap_time;
         lap_time_text.text = "LAP TIME: " + lap_time.ToString();
+
+        current_speed = car_controller.CurrentSpeed;
+        speed_text.text = ((int)current_speed).ToString() + " km/h";
     }
 }
